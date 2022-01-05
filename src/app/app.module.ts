c372a3fe6eeb8c050d7a4fd6e93570151f088e74
Tomasz Prusak <tomasz@clubforce.com>
2022-01-05 14:04:57 +0000

    dynamic-forms-demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,9 @@ import { myDataService } from './shared/services/myData.service';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { FilterPipe } from 'src/app/shared/pipes/filterSearch.pipe';
-import { TableComponent } from './pages/table/table.component';
 import { MenuComponent } from './menu/menu.component';
 import { HomeComponent } from './pages/home/home.component';
-import { ProgramikiComponent } from './pages/programiki/programiki.component';
-import { ApiServicesComponent } from './pages/api-services/api-services.component';
-import { ApiNGRXEntityComponent } from './pages/api-ngrxentity/api-ngrxentity.component';
-import { ApiNGRXDataComponent } from './pages/api-ngrxdata/api-ngrxdata.component';
 import { ChartJSComponent } from './views/chart-js/chart-js.component';
-import { ChartsComponent } from './pages/charts/charts.component';
 import { ThemeService } from 'ng2-charts';
 import { NgChartsModule } from 'ng2-charts';
 import { DynamicFormComponent } from './pages/dynamic-form/dynamic-form.component';
@@ -29,15 +23,9 @@ import { ReactiveFormsModule} from '@angular/forms';
   declarations: [
     AppComponent,
     FilterPipe,
-    TableComponent,
     MenuComponent,
     HomeComponent,
-    ProgramikiComponent,
-    ApiServicesComponent,
-    ApiNGRXEntityComponent,
-    ApiNGRXDataComponent,
     ChartJSComponent,
-    ChartsComponent,
     DynamicFormComponent,
     UiInputFormComponent,
   ],
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,13 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
-import { ProgramikiComponent } from './pages/programiki/programiki.component';
-import { TableComponent } from './pages/table/table.component';
 import { HomeComponent } from './pages/home/home.component';
-import { ApiServicesComponent } from './pages/api-services/api-services.component';
-import { ApiNGRXEntityComponent } from './pages/api-ngrxentity/api-ngrxentity.component';
-import { ApiNGRXDataComponent } from './pages/api-ngrxdata/api-ngrxdata.component';
-import { ChartsComponent } from './pages/charts/charts.component';
 import { DynamicFormComponent } from './pages/dynamic-form/dynamic-form.component';
 
 
@@ -15,24 +9,6 @@ const routes: Routes = [
 
   {
     path: "", component: HomeComponent
-},
-{
-    path: "table", component: TableComponent
-},
-{
-  path: "programiki", component: ProgramikiComponent
-},
-{
-  path: "api-services", component: ApiServicesComponent
-},
-{
-  path: "api-ngrx-entity", component: ApiNGRXEntityComponent
-},
-{
-  path: "api-ngrx-data", component: ApiNGRXDataComponent
-},
-{
-  path: "charts", component: ChartsComponent
 },
 {
   path: "dynamic-forms", component: DynamicFormComponent
